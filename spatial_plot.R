149c04b2f394e9598fe539dcf2558aa7bd05f258
Willie Cohen <wcohen22@amherst.edu>
2021-05-05 22:05:04 -0400

    .

--- spatial_plot.R
+++ spatial_plot.R
@@ -17,13 +17,18 @@ testSpatial <- spatial_data %>%
                           (72<angle & angle <90) ~ "5"))
 
 
+
 genericStadium <- MLBStadiumsPathData %>%
-  filter(team == "generic")
+  filter(team == "generic") 
   
 
 g <- ggplot(data = testSpatial, aes(x = hc_x, y = hc_y)) +
-    geom_point() +
+    #geom_point() +
     geom_mlb_stadium(stadium_ids = "generic", stadium_segments = "all") +
-    scale_y_reverse()
+    scale_y_reverse() + 
+    #geom_segment(aes(x = 125, y = 208, xend = 90, yend = tan(63*pi/180)*25)) +
+    geom_segment(aes(x = 125, y = 208, xend = 115, yend =  208-tan(81*pi/180)*10)) +
+    geom_segment(aes(x = 125, y = 208, xend = 150, yend = 208 + tan(99*pi/180)*25)) 
+  
 
 g
